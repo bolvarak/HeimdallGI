5aa81b50ac5f440155a216c6dd9c1f073cb8a2d3
Travis Brown <tmbrown6@gmail.com>
2014-08-24 17:39:47 -0400

    Adding HTTP

---
+++ HTTP.h
@@ -0,0 +1,22 @@
+#ifndef HEIMDALLGI_HTTP_H
+#define HEIMDALLGI_HTTP_H
+
+#include <QObject>
+
+namespace HeimdallGI {
+
+	class HTTP : public QObject
+	{
+			Q_OBJECT
+		public:
+			explicit HTTP(QObject *parent = 0);
+
+		signals:
+
+		public slots:
+
+	};
+
+} // namespace HeimdallGI
+
+#endif // HEIMDALLGI_HTTP_H
