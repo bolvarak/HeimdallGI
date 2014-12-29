9e0c8c7f44758dfecdd500e80eeb79b4fa331c36
Travis Brown <tbrown@myhrdesk.com>
2014-12-29 17:47:24 -0500

    Finalizing MaxMind integration

--- MaxMind.cpp
+++ MaxMind.cpp
@@ -1,15 +1,200 @@
+///////////////////////////////////////////////////////////////////////////////
+/// Headers //////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+
 #include "MaxMind.h"
+
+///////////////////////////////////////////////////////////////////////////////
+/// HeimdallGI Namespace /////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
 
 namespace HeimdallGI {
 
-MaxMind::MaxMind(QObject *parent) : QObject(parent){
+	///////////////////////////////////////////////////////////////////////////
+	/// Globals //////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////
+
+	MaxMind* MaxMind::mInstance = NULL;
+
+	///////////////////////////////////////////////////////////////////////////
+	/// Singleton ////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////
+
+	MaxMind* MaxMind::Instance(bool blnReset) {
+		// Check for an existing instance or a reset flag
+		if ((mInstance == NULL) || (blnReset == true)) {
+			// Create a new instance
+			mInstance = new MaxMind;
+		}
+		// Return the instance
+		return mInstance;
+	}
+
+	///////////////////////////////////////////////////////////////////////////
+	/// Constructor //////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////
+
+	MaxMind::MaxMind(QObject* qoParent) : QObject(qoParent) {
+		// Reset the city database
+		this->mCityDatabase = NULL;
+		// Reset the ISP database
+		this->mIspDatabase  = NULL;
+	}
+
+	///////////////////////////////////////////////////////////////////////////
+	/// Destructor ///////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////
+
+	MaxMind::~MaxMind() {
+		// Close the database streams
+		this->closeConnection();
+	}
+
+	///////////////////////////////////////////////////////////////////////////
+	/// Protected Methods ////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////
+
+	void MaxMind::closeDB(GeoIP* gipDatabase) {
+		// Check to see if the DB is open
+		if (this->dbIsOpen(gipDatabase)) {
+			// Delete the database stream
+			GeoIP_delete(gipDatabase);
+			// Reset the database
+			gipDatabase = NULL;
+		}
+	}
+
+	bool MaxMind::dbIsOpen(GeoIP* gipDatabase) {
+		// Return the open status
+		return gipDatabase != NULL;
+	}
+
+	bool MaxMind::openDB(GeoIP** gipDatabase, const QString &strFileName) {
+		// Open the stream to the database
+		*gipDatabase = GeoIP_open(strFileName.toLocal8Bit().constData(), GEOIP_MMAP_CACHE);
+		// Make sure the connection is open
+		if (this->dbIsOpen(*gipDatabase)) {
+			// Set the database charset
+			GeoIP_set_charset(*gipDatabase, GEOIP_CHARSET_UTF8);
+			// We're done
+			return true;
+		}
+		// We're done
+		return false;
+	}
+
+	///////////////////////////////////////////////////////////////////////////
+	/// Public Methods ///////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////
+
+	void MaxMind::closeConnection() {
+		// Close the city database
+		this->closeDB(this->mCityDatabase);
+		// Close the ISP database
+		this->closeDB(this->mIspDatabase);
+	}
+
+	GeoRecord MaxMind::findByAddress(const QHostAddress &qhaIpAddress) {
+		// Make sure the city database is open
+		if (this->dbIsOpen(this->mCityDatabase) && !qhaIpAddress.isNull()) {
+			// Create the ISP name placeholder
+			QString strISP = QString::null;
+			const char* chrIspName;
+			// Create the record placeholder
+			GeoIPRecord* girResponse;
+			// Check for IPv6
+			if (qhaIpAddress.protocol() == QAbstractSocket::IPv6Protocol) {
+				// Grab the record
+				girResponse = GeoIP_record_by_addr_v6(this->mCityDatabase, qhaIpAddress.toString().toLatin1().constData());
+			} else {
+				// Grab the record
+				girResponse = GeoIP_record_by_addr(this->mCityDatabase, qhaIpAddress.toString().toLatin1().constData());
+			}
+			// Make sure the ISP database is open
+			if (this->dbIsOpen(this->mIspDatabase)) {
+				// Check for IPv6
+				if (qhaIpAddress.protocol() == QAbstractSocket::IPv6Protocol) {
+					// Grab the ISP name
+					chrIspName = GeoIP_org_by_name_v6(this->mIspDatabase, qhaIpAddress.toString().toLatin1().constData());
+				} else {
+					// Grab the ISP name
+					chrIspName = GeoIP_org_by_name(this->mIspDatabase, qhaIpAddress.toString().toLatin1().constData());
+				}
+				// Make sure we have a name
+				if (chrIspName != NULL) {
+					// Convert the ISP name to QString
+					strISP = QString::fromUtf8(chrIspName);
+					// Clear the ISP name from memory
+					free((char *) chrIspName);
+				}
+			} else {
+				// Send a warning
+				qWarning("HeimdallGI::MaxMind::findByAddress(): MaxMind ISP Database Not Open");
+				// Return an empty structure
+				return GeoRecord();
+			}
+			// Make sure we have a record to return
+			if (girResponse != NULL) {
+				// Set the time zone
+				QString strTimeZone       = QString::fromUtf8(GeoIP_time_zone_by_country_and_region(girResponse->country_code, girResponse->region));
+				// Set the country
+				QString strCountry        = QString::fromUtf8(girResponse->country_name);
+				// Set the city
+				QString strCity           = QString::fromUtf8(girResponse->city);
+				// Set the region placeholders
+				QString strRegion         = QString::null;
+				const char* chrRegionName = GeoIP_region_name_by_code(girResponse->country_code, girResponse->region);
+				// Make sure we have a region name
+				if (chrRegionName != NULL) {
+					// Set the region
+					strRegion = QString::fromUtf8(chrRegionName);
+				}
+				// Create the response structure
+				GeoRecord geoRecord(
+					strCity,
+					QString::fromUtf8(girResponse->continent_code),
+					strCountry,
+					QString::fromUtf8(girResponse->country_code),
+					strISP,
+					girResponse->latitude,
+					girResponse->longitude,
+					strRegion,
+					strTimeZone
+				);
+				// Delete the response to free up some memory
+				GeoIPRecord_delete(girResponse);
+				// Return the response
+				return geoRecord;
+			}
+		}
+		// Send a warning
+		qWarning("HeimdallGI::MaxMind::findByAddress(): MaxMind City Database Not Open");
+		// Return an empty structure
+		return GeoRecord();
+	}
+
+	bool MaxMind::openConnection(const QString &strCityDatabasePath, const QString &strIspDatabasePath) {
+		// Make sure we can open the city database
+		if (!this->openDB(&this->mCityDatabase, strCityDatabasePath)) {
+			// Throw a critical error
+			qCritical("Could not open the City Database: %s", qPrintable(strCityDatabasePath));
+			// We're done
+			return false;
+		}
+		// Make sure we can open the ISP database
+		if (!this->openDB(&this->mIspDatabase, strIspDatabasePath)) {
+			// Throw a critical error
+			qCritical("Could not open the ISP Database: %s", qPrintable(strIspDatabasePath));
+			// We're done
+			return false;
+		}
+		// We're done
+		return true;
+	}
+
+///////////////////////////////////////////////////////////////////////////////
+/// End HeimdallGI Namespace /////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
 
 }
-
-MaxMind::~MaxMind()
-{
-
-}
-
-} // namespace HeimdallGI
 
--- MaxMind.h
+++ MaxMind.h
@@ -23,6 +23,285 @@
 namespace HeimdallGI {
 
 	///////////////////////////////////////////////////////////////////////////
+	/// Structures ///////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////
+
+
+	struct GeoRecord {
+
+			///////////////////////////////////////////////////////////////////
+			/// Properties ///////////////////////////////////////////////////
+			/////////////////////////////////////////////////////////////////
+
+			/**
+			 * @paragraph This property contains the city of the IP address
+			 * @brief HeimdallGI::GeoRecord::mCity
+			 * @var QString
+			 */
+			QString mCity;
+
+			/**
+			 * @paragraph This property contains the ISO-3166-1-A2 country code of the IP address
+			 * @brief HeimdallGI::GeoRecord::mContinentISO
+			 * @var QString
+			 */
+			QString mContinentISO;
+
+			/**
+			 * @paragraph This property contains the country of the IP address
+			 * @brief HeimdallGI::GeoRecord::mCountry
+			 * @var QString
+			 */
+			QString mCountry;
+
+			/**
+			 * @paragraph This property contains the ISO-3166-1-A2 country code of the IP address
+			 * @brief HeimdallGI::GeoRecord::mCountryISO
+			 * @var QString
+			 */
+			QString mCountryISO;
+
+			/**
+			 * @paragraph This property contains the Internet Service Provider (ISP) of the IP address
+			 * @brief HeimdallGI::GeoRecord::mISP
+			 * @var QString
+			 */
+			QString mISP;
+
+			/**
+			 * @paragraph This property contains the latitude of the IP address
+			 * @brief HeimdallGI::GeoRecord::mLatitude
+			 * @var float
+			 */
+			float mLatitude;
+
+			/**
+			 * @paragraph This property contains the longitude of the IP address
+			 * @brief HeimdallGI::GeoRecord::mLongitude
+			 * @var float
+			 */
+			float mLongitude;
+
+			/**
+			 * @paragraph This property contains the region or stat of the IP address
+			 * @brief HeimdallGI::GeoRecord::mRegion
+			 * @var QString
+			 */
+			QString mRegion;
+
+			/**
+			 * @paragraph This property contains the time zone of the IP address
+			 * @brief HeimdallGI::GeoRecord::mTimeZone
+			 * @var QString
+			 */
+			QString mTimeZone;
+
+			///////////////////////////////////////////////////////////////////
+			/// Constructors /////////////////////////////////////////////////
+			/////////////////////////////////////////////////////////////////
+
+			/**
+			 * @paragraph This constructor creates an empty HeimdallGI::GeoRecord
+			 * @brief HeimdallGI::GeoRecord()
+			 * @return HeimdallGI::GeoRecord
+			 */
+			GeoRecord() {}
+
+			/**
+			 * @paragraph This constructor builds a complete HeimdallGI::GeoRecord
+			 * @brief HeimdallGI::GeoRecord()
+			 * @param QString strCity
+			 * @param QString strContinentISO
+			 * @param QString strCountry
+			 * @param QString strCountryISO
+			 * @param QString strISP
+			 * @param float intLatitude
+			 * @param float intLongitude
+			 * @param QString strRegion
+			 * @param QString strTimeZone
+			 * @return HeimdallGI::GeoRecord
+			 */
+			GeoRecord(
+				QString strCity,
+				QString strContinentISO,
+				QString strCountry,
+				QString strCountryISO,
+				QString strISP,
+				float intLatitude,
+				float intLongitude,
+				QString strRegion,
+				QString strTimeZone
+			) :
+				mCity(strCity),
+				mContinentISO(strContinentISO),
+				mCountry(strCountry),
+				mCountryISO(strCountryISO),
+				mISP(strISP),
+				mLatitude(intLatitude),
+				mLongitude(intLongitude),
+				mRegion(strRegion),
+				mTimeZone(strTimeZone)
+			{
+				// Minimize memory usage
+				mCity.squeeze();
+				mContinentISO.squeeze();
+				mCountry.squeeze();
+				mCountryISO.squeeze();
+				mISP.squeeze();
+				mRegion.squeeze();
+				mTimeZone.squeeze();
+			}
+
+			///////////////////////////////////////////////////////////////////
+			/// Getters //////////////////////////////////////////////////////
+			/////////////////////////////////////////////////////////////////
+
+			/**
+			 * @paragraph This method returns the IP's city
+			 * @brief HeimdallGI::GeoRecord::getCity()
+			 * @return QString HeimdallGI::GeoRecord::mCity
+			 */
+			QString getCity()         { return mCity;         }
+
+			/**
+			 * @paragraph This method returns the IP's continent ISO code
+			 * @brief HeimdallGI::GeoRecord::getContinentISO()
+			 * @return QString HeimdallGI::GeoRecord::mContinentISO
+			 */
+			QString getContinentISO() { return mContinentISO; }
+
+			/**
+			 * @paragraph This method returns the IP's country
+			 * @brief HeimdallGI::GeoRecord::getCountry()
+			 * @return QString HeimdallGI::GeoRecord::mCountry
+			 */
+			QString getCountry()      { return mCountry;      }
+
+			/**
+			 * @paragraph This method returns the IP's ISO country code
+			 * @brief HeimdallGI::GeoRecord::getCountryISO()
+			 * @return QString HeimdallGI::GeoRecord::mCountryISO
+			 */
+			QString getCountryISO()   { return mCountryISO;   }
+
+			/**
+			 * @paragraph This method returns the IP's ISP
+			 * @brief HeimdallGI::GeoRecord::getISP()
+			 * @return QString HeimdallGI::GeoRecord::mISP
+			 */
+			QString getISP()          { return mISP;          }
+
+			/**
+			 * @paragraph This method returns the IP's latitude
+			 * @brief HeimdallGI::GeoRecord::getLatitude()
+			 * @return float HeimdallGI::GeoRecord::mLatitude
+			 */
+			float getLatitude()       { return mLatitude;     }
+
+			/**
+			 * @paragraph This method returns the IP's longitude
+			 * @brief HeimdallGI::GeoRecord::getLongitude()
+			 * @return float HeimdallGI::GeoRecord::mLongitude
+			 */
+			float getLongitude()      { return mLongitude;    }
+
+			/**
+			 * @paragraph This method returns the IP's region/state
+			 * @brief HeimdallGI::GeoRecord::getRegion()
+			 * @return QString HeimdallGI::GeoRecord::mRegion
+			 */
+			QString getRegion()       { return mRegion;       }
+
+			/**
+			 * @paragraph This method returns the IP's timezone
+			 * @brief HeimdallGI::GeoRecord::getTimeZone()
+			 * @return QString HeimdallGI::GeoRecord::mTimeZone
+			 */
+			QString getTimeZone()     { return mTimeZone;     }
+
+			///////////////////////////////////////////////////////////////////
+			/// Setters //////////////////////////////////////////////////////
+			/////////////////////////////////////////////////////////////////
+
+			/**
+			 * @paragraph This method sets the IP's city
+			 * @brief HeimdallGI::GeoRecord::setCity()
+			 * @param QString strCity
+			 * @return void
+			 */
+			void setCity(QString strCity)                 { mCity         = strCity;         mCity.squeeze();         }
+
+			/**
+			 * @paragraph This method sets the IP's continent ISO code
+			 * @brief HeimdallGI::GeoRecord::setContinentISO()
+			 * @param QString strContinentISO
+			 * @return void
+			 */
+			void setContinentISO(QString strContinentISO) { mContinentISO = strContinentISO; mContinentISO.squeeze(); }
+
+			/**
+			 * @paragraph This method sets the IP's country
+			 * @brief HeimdallGI::GeoRecord::setCountry()
+			 * @param QString strCountry
+			 * @return void
+			 */
+			void setCountry(QString strCountry)           { mCountry      = strCountry;      mCountry.squeeze();      }
+
+			/**
+			 * @paragraph This method sets the IP's country ISO code
+			 * @brief HeimdallGI::GeoRecord::setCountryISO()
+			 * @param QString strCountryISO
+			 * @return void
+			 */
+			void setCountryISO(QString strCountryISO)     { mCountryISO   = strCountryISO;   mCountryISO.squeeze();   }
+
+			/**
+			 * @paragraph This method sets the IP's Internet Service Provider (ISP)
+			 * @brief HeimdallGI::GeoRecord::setISP()
+			 * @param QString strISP
+			 * @return void
+			 */
+			void setISP(QString strISP)                   { mISP          = strISP;          mISP.squeeze();          }
+
+			/**
+			 * @paragraph This method sets the IP's latitude
+			 * @brief HeimdallGI::GeoRecord::setLatitude()
+			 * @param float intLatitude
+			 * @return void
+			 */
+			void setLatitude(float intLatitude)           { mLatitude     = intLatitude;                              }
+
+			/**
+			 * @paragraph This method sets the IP's longitude
+			 * @brief HeimdallGI::GeoRecord::setLongitude()
+			 * @param float intLongitude
+			 * @return void
+			 */
+			void setLongitude(float intLongitude)         { mLongitude    = intLongitude;                             }
+
+			/**
+			 * @paragraph This method sets the IP's region
+			 * @brief HeimdallGI::GeoRecord::setRegion()
+			 * @param QString strRegion
+			 * @return void
+			 */
+			void setRegion(QString strRegion)             { mRegion       = strRegion;       mRegion.squeeze();       }
+
+			/**
+			 * @paragraph This method sets the IP's time zone
+			 * @brief HeimdallGI::GeoRecord::setTimeZone()
+			 * @param QString strTimeZone
+			 * @return void
+			 */
+			void setTimeZone(QString strTimeZone)         { mTimeZone     = strTimeZone;     mTimeZone.squeeze();     }
+	};
+
+	/**
+	 * Register the GeoRecord to allow testing
+	 */
+	// Q_DECLARE_METATYPE(HeimdallGI::GeoRecord)
+
+	///////////////////////////////////////////////////////////////////////////
 	/// HeimdallGI::MaxMind Class Definition /////////////////////////////////
 	/////////////////////////////////////////////////////////////////////////
 
@@ -50,14 +329,87 @@ namespace HeimdallGI {
 			 */
 			static MaxMind* mInstance;
 
+			/**
+			 * @paragraph This property contains the reference to our City Database
+			 * @brief HeimdallGI::MaxMind::mCityDatabase
+			 * @var GeoIP*
+			 */
+			GeoIP* mCityDatabase;
 
-			GeoIP
+			/**
+			 * @paragraph This property contains the reference to our ISP Database
+			 * @brief HeimdallGI::MaxMind::mIspDatabase
+			 * @var GeoIP*
+			 */
+			GeoIP* mIspDatabase;
+
+			///////////////////////////////////////////////////////////////////
+			/// Methods //////////////////////////////////////////////////////
+			/////////////////////////////////////////////////////////////////
+
+			/**
+			 * @paragraph This method closes a database
+			 * @brief HeimdallGI::MaxMind::closeDB()
+			 * @param GeoIP* gipDatabase
+			 * @protected
+			 * @return void
+			 */
+			void closeDB(GeoIP* gipDatabase);
+
+			/**
+			 * @paragraph This method determines if a database is open
+			 * @brief HeimdallGI::MaxMind::dbIsOpen()
+			 * @param GeoIP* const gipDatabase
+			 * @protected
+			 * @return bool
+			 */
+			bool dbIsOpen(GeoIP* gipDatabase);
+
+			/**
+			 * @paragraph This method returns the database type
+			 * @brief HeimdallGI::MaxMind::dbType()
+			 * @protected
+			 * @return HeimdallGI::MaxMind::DatabaseType
+			 */
+			// DatabaseType dbType() const;
+
+			/**
+			 * @paragraph This method opens a database file
+			 * @brief HeimdallGI::MaxMind::openDB()
+			 * @param GeoIP** gipDatabase
+			 * @param const QString strFileName
+			 * @protected
+			 * @return bool
+			 */
+			bool openDB(GeoIP** gipDatabase, const QString &strFileName);
 
 		///////////////////////////////////////////////////////////////////////
 		/// Public Methods & Properties //////////////////////////////////////
 		/////////////////////////////////////////////////////////////////////
 
 		public:
+
+			///////////////////////////////////////////////////////////////////
+			/// Enumerations /////////////////////////////////////////////////
+			/////////////////////////////////////////////////////////////////
+
+			/**
+			 * @paragraph This enumeration contains the MaxMind database types relative to this interface
+			 * @brief HeimdallGI::MaxMind::DatabaseType
+			 * @var enum
+			 */
+			enum DatabaseType {
+				ASN,
+				City,
+				Country,
+				Domain,
+				ISP,
+				NetSpeed,
+				Organization,
+				Proxy,
+				Region,
+				Unknown = 0
+			};
 
 			///////////////////////////////////////////////////////////////////
 			/// Singleton ////////////////////////////////////////////////////
@@ -83,6 +435,35 @@ namespace HeimdallGI {
 			/////////////////////////////////////////////////////////////////
 
 			~MaxMind();
+
+			///////////////////////////////////////////////////////////////////
+			/// Methods //////////////////////////////////////////////////////
+			/////////////////////////////////////////////////////////////////
+
+			/**
+			 * @paragraph This method closes the connection stream to the databases
+			 * @brief HeimdallGI::MaxMind::closeConnection()
+			 * @return void
+			 */
+			void closeConnection();
+
+			/**
+			 * @paragraph This method searches for a record by the IPv4 or IPv6 address
+			 * @brief HeimdallGI::MaxMind::findByAddress()
+			 * @param const QHostAddress qhaIpAddress
+			 * @return HeimdallGI::GeoRecord
+			 */
+			GeoRecord findByAddress(const QHostAddress &qhaIpAddress);
+
+			/**
+			 * @paragraph This method opens a connection stream to the MaxMind databases
+			 * @brief HeimdallGI::MaxMind::openConnection()
+			 * @param const QString strCityDatabasePath
+			 * @param const QString strIspDatabasePath
+			 * @return bool
+			 */
+			bool openConnection(const QString &strCityDatabasePath, const QString &strIspDatabasePath);
+
 
 	///////////////////////////////////////////////////////////////////////////
 	/// End HeimdallGI::MaxMind Class Definition /////////////////////////////
