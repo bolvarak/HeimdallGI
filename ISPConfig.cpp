0de1a4bf2d8d3d1d595692dc450a0ec8f088066d
Travis Brown <tbrown@myhrdesk.com>
2014-12-30 20:07:03 -0500

    Updating

---
+++ ISPConfig.cpp
@@ -0,0 +1,16 @@
+#include "ISPConfig.h"
+
+namespace HeimdallGI {
+
+ISPConfig::ISPConfig(QObject *parent) : QObject(parent)
+{
+
+}
+
+ISPConfig::~ISPConfig()
+{
+
+}
+
+} // namespace HeimdallGI
+
