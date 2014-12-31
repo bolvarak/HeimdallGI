8b96d70c6998ff5c6e963897d06d826ceb58c04a
Travis Brown <tbrown@myhrdesk.com>
2014-12-31 15:31:55 -0500

    Updating

--- ISPConfig.cpp
+++ ISPConfig.cpp
@@ -1,16 +1,188 @@
+///////////////////////////////////////////////////////////////////////////////
+/// Headers //////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+
 #include "ISPConfig.h"
+
+///////////////////////////////////////////////////////////////////////////////
+/// HeimdallGI Namespace /////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
 
 namespace HeimdallGI {
 
-ISPConfig::ISPConfig(QObject *parent) : QObject(parent)
-{
+	///////////////////////////////////////////////////////////////////////////
+	/// Globals //////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////
+
+	ISPConfig* mInstance = NULL;
+
+	///////////////////////////////////////////////////////////////////////////
+	/// Singleton ////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////
+
+	ISPConfig* ISPConfig::Instance(bool blnReset) {
+		// Check for an existing instance or a reset flag
+		if ((mInstance == NULL) || (blnReset == true)) {
+			// Create a new instance
+			mInstance = new ISPConfig;
+		}
+		// Return the instance
+		return mInstance;
+	}
+
+	///////////////////////////////////////////////////////////////////////////
+	/// Constructor //////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////
+
+	ISPConfig::ISPConfig(QObject *qoParent) : QObject(qoParent) {
+		// Connect the response signal
+		connect(&mConnection, SIGNAL(responseReady(), this, SLOT(getSoapResponse());
+		// Default the error flag
+		this->mErrorFlag       = false;
+		// Default the error message
+		this->mErrorMessage    = QString::null;
+		// Default the secure transport flag
+		this->mSecureTransport = false;
+	}
+
+	///////////////////////////////////////////////////////////////////////////
+	/// Destructor ///////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////
+
+	ISPConfig::~ISPConfig() {}
+
+	///////////////////////////////////////////////////////////////////////////
+	/// Protected Methods ////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////
+
+	void ISPConfig::makeRequest(QString strMethod, QVariantMap qvmArguments) {
+		// Reset the error state
+		this->mErrorFlag    = false;
+		this->mErrorMessage = QString::null;
+		// Create the SOAP message
+		QtSoapMessage qsmRequest;
+		// Set the method
+		qsmRequest.setMethod(strMethod, Configuration::Get("ispConfig.uri").toString());
+		// Add the arguments
+		this->mapToArguments(qsmRequest, qvmArguments);
+		// Set the host
+		this->mConnection.setHost(Configuration::Get("ispConfig.host").toString(), this->mSecureTransport, Configuration::Get("ispConfig.port").toInt());
+		// Set the action
+		this->mConnection.setAction(QString("%1/%2").arg(Configuration::Get("ispConfig.uri").toString(), strMethod));
+		// Submit the request
+		this->mConnection.submitRequest(qsmRequest, "/remote/index.php");
+	}
+
+	void ISPConfig::makeRequest(QtSoapQName qsnMethod, QVariantMap qvmArguments) {
+		// Reset the error state
+		this->mErrorFlag    = false;
+		this->mErrorMessage = QString::null;
+		// Create the SOAP message
+		QtSoapMessage qsmRequest;
+		// Set the method
+		qsmRequest.setMethod(qsnMethod);
+		// Add the arguments
+		this->mapToArguments(qsmRequest, qvmArguments);
+		// Set the host
+		this->mConnection.setHost(Configuration::Get("ispConfig.host").toString(), this->mSecureTransport, Configuration::Get("ispConfig.port").toInt());
+		// Set the action
+		this->mConnection.setAction(QString("%1/%2").arg(Configuration::Get("ispConfig.uri").toString(), qsnMethod.name()));
+		// Submit the request
+		this->mConnection.submitRequest(qsmRequest, "/remote/index.php");
+	}
+
+	void ISPConfig::makeRequest(QtSoapMessage qsmRequest) {
+		// reset the error state
+		this->mErrorFlag    = false;
+		this->mErrorMessage = QString::null;
+		// Set the host
+		this->mConnection.setHost(Configuration::Get("ispConfig.host").toString(), this->mSecureTransport, Configuration::Get("ispConfig.port").toInt());
+		// Set the action
+		this->mConnection.setAction(QString("%1/%2").arg(Configuration::Get("ispConfig.uri"), qsmRequest.method().name()));
+		// Submit the request
+		this->mConnection.submitRequest(qsmRequest, "/remote/index.php");
+	}
+
+	void ISPConfig::mapToArguments(QtSoapMessage &qsmRequest, QVariantMap qvmArguments) {
+		// Iterate over the arguments
+		for (QVariantMap::const_iterator itrArguments = qvmArguments.constBegin(); itrArguments != qvmArguments.constEnd(); ++itrArguments) {
+			// Check the type
+			if (itrArguments.value().canConvert(QMetaType::Bool)) {       // Boolean
+				// Add the argument
+				qsmRequest.addMethodArgument(itrArguments.key(), "", itrArguments.value().toBool());
+			} else if (itrArguments.value().canConvert(QMetaType::Int)) { // Integer
+				// Add the argument
+				qsmRequest.addMethodArgument(itrArguments.key(), "", itrArguments.value().toInt());
+			} else {                                                      // String
+				// Add the argument
+				qsmRequest.addMethodArgument(itrArguments.key(), "", itrArguments.value().toString());
+			}
+		}
+	}
+
+	void ISPConfig::sendLoginResponse() {
+		// Empty the response
+		this->mResponse.clear();
+		// Set the session ID
+		this->mResponse.insert("sessionID", this->mConnection.getResponse().returnValue()["session_id"].toString());
+		// We're done, emit the signal
+		emit this->startSessionResponseReady(this->mResponse);
+	}
+
+	void ISPConfig::sendLogoutResponse() {
+		// Empty the response
+		this->mResponse.clear();
+		// We're done, emit the signal
+		emit this->endSessionResponseReady(this->mResponse);
+	}
+
+	///////////////////////////////////////////////////////////////////////////
+	/// Protected Slots //////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////
+
+	void ISPConfig::getSoapResponse() {
+		// Determine the method
+		switch(this->mCurrentMethod) {
+			// Login
+			case SoapMethod::Login  : return this->sendLoginResponse();  break;
+			// Logout
+			case SoapMethod::Logout : return this->sendLogoutResponse(); break;
+		}
+	}
+
+	///////////////////////////////////////////////////////////////////////////
+	/// Public Methods ///////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////
+
+
+	bool ISPConfig::endSession() {}
+
+
+	bool ISPConfig::startSession(QString strUsername, QString strPassword) {
+		// Check for local username
+		if (strUsername == QString::null) {
+			// Set the username from the configuration
+			strUsername = Configuration::Get("ispConfig.userName").toString();
+		}
+		// Check for local password
+		if (strPassword == QString::null) {
+			// Set the password from the configuration
+			strPassword = Configuration::Get("ispConfig.passWord").toString();
+		}
+		// Set the current executing method
+		this->mCurrentMethod = SoapMethod::Login;
+		// Send the request
+		this->makeRequest("login", {
+			"username" = strUsername,
+			"password" = strPassword
+		});
+		// Wait for the response
+	}
+
+
+///////////////////////////////////////////////////////////////////////////////
+/// End HeimdallGI Namespace /////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
 
 }
-
-ISPConfig::~ISPConfig()
-{
-
-}
-
-} // namespace HeimdallGI
 
--- ISPConfig.h
+++ ISPConfig.h
@@ -15,7 +15,8 @@
 #include "QVariant"
 #include "QVariantList"
 #include "QVariantMap"
-
+#include "qtsoap.h"
+#include "Configuration.h"
 
 ///////////////////////////////////////////////////////////////////////////////
 /// HeimdallGI Namespace /////////////////////////////////////////////////////
@@ -59,6 +60,41 @@ namespace HeimdallGI {
 			QtSoapHttpTransport mConnection;
 
 			/**
+			 * @paragraph This property contains the current executing method
+			 * @brief HeimdallGI::ISPConfig::mCurrentMethod
+			 * @var HeimdallGI::ISPConfig::SoapMethod
+			 */
+			SoapMethod mCurrentMethod;
+
+			/**
+			 * @paragraph This property tells the interface whether or not an error occurred
+			 * @brief HeimdallGI::ISPConfig::mErrorFlag
+			 * @var bool
+			 */
+			bool mErrorFlag;
+
+			/**
+			 * @paragraph This property contains the last error message
+			 * @brief HeimdallGI::ISPConfig::mErrorMessage
+			 * @var QString
+			 */
+			QString mErrorMessage;
+
+			/**
+			 * @paragraph This property contains the response to the caller
+			 * @brief HeimdallGI::ISPConfig::mResponse
+			 * @var QVariantMap
+			 */
+			QVariantMap mResponse;
+
+			/**
+			 * @paragraph This property tells the interface whether or not to use SSL
+			 * @brief HeimdallGI::ISPConfig::mSecureTransport
+			 * @var bool
+			 */
+			bool mSecureTransport;
+
+			/**
 			 * @paragraph This property contains the current session ID
 			 * @brief HeimdallGI::ISPConfig::mSessionID
 			 * @var QString
@@ -95,11 +131,65 @@ namespace HeimdallGI {
 			 */
 			void makeRequest(QtSoapMessage qsmRequest);
 
+			/**
+			 * @paragraph This method converts a QVariantMap to SOAP arguments
+			 * @brief HeimdallGI::ISPConfig::mapToArguments()
+			 * @param QtSoapMessage qsmRequest
+			 * @param QVariantMap qvmArguments
+			 * @return void
+			 */
+			void mapToArguments(QtSoapMessage &qsmRequest, QVariantMap qvmArguments);
+
+			/**
+			 * @paragraph This method processes the login response
+			 * @brief HeimdallGI::ISPConfig::sendLoginResponse()
+			 * @emits HeimdallGI::ISPConfig::startSessionResponseReady()
+			 * @return void
+			 */
+			void sendLoginResponse();
+
+			/**
+			 * @paragraph This method processes the logout response
+			 * @brief HeimdallGI::ISPConfig::sendLogoutResponse()
+			 * @emits HeimdallGI::ISPConfig::endSessionResponseReady()
+			 * @return void
+			 */
+			void sendLogoutResponse();
+
+		///////////////////////////////////////////////////////////////////////
+		/// Protected Slots //////////////////////////////////////////////////
+		/////////////////////////////////////////////////////////////////////
+
+		protected slots:
+
+			/**
+			 * @paragraph This slot is executed when the SOAP response is ready
+			 * @brief HeimdallGI::ISPConfig::getSoapResponse()
+			 * @return void
+			 */
+			void getSoapResponse();
+
 		///////////////////////////////////////////////////////////////////////
 		/// Public Methods & Properties //////////////////////////////////////
 		/////////////////////////////////////////////////////////////////////
 
 		public:
+
+			///////////////////////////////////////////////////////////////////
+			/// Enumerations /////////////////////////////////////////////////
+			/////////////////////////////////////////////////////////////////
+
+			/**
+			 * @paragraph This enumeration contains method definitions
+			 * @brief HeimdallGI::ISPConfig::SoapMethod
+			 * @var enum
+			 */
+			enum SoapMethod {
+				None    = 0x000,
+				Unknown = 0x001,
+				Login   = 0x002,
+				Logout  = 0x003
+			};
 
 			///////////////////////////////////////////////////////////////////
 			/// Singleton ////////////////////////////////////////////////////
@@ -143,7 +233,129 @@ namespace HeimdallGI {
 			 * @param QString strPassword
 			 * @return bool
 			 */
-			bool startSession(QString strUsername, QString strPassword);
+			bool startSession(QString strUsername = QString::null, QString strPassword = QString::null);
+
+			///////////////////////////////////////////////////////////////////
+			/// Getters //////////////////////////////////////////////////////
+			/////////////////////////////////////////////////////////////////
+
+			/**
+			 * @paragraph This method returns the current executing SOAP method
+			 * @brief HeimdallGI::ISPConfig::getCurrentMethod()
+			 * @return HeimdallGI::ISPConfig::SoapMethod HeimdallGI::ISPConfig::mCurrentMethod
+			 */
+			SoapMethod getCurrentMethod() { return this->mCurrentMethod;           }
+
+			/**
+			 * @paragraph This method returns the error flag
+			 * @brief HeimdallGI::ISPConfig::getErrorFlag()
+			 * @return bool HeimdallGI::ISPConfig::mErrorFlag
+			 */
+			bool getErrorFlag()            { return this->mErrorFlag;                }
+
+			/**
+			 * @paragraph This method returns the last error message
+			 * @brief HeimdallGI::ISPConfig::getErrorMessage()
+			 * @return QString HeimdallGI::ISPConfig::mErrorMessage
+			 */
+			QString getErrorMessage()      { return this->mErrorMessage;             }
+
+			/**
+			 * @paragraph This method returns the last SOAP response from the ISPConfig server
+			 * @brief HeimdallGI::ISPConfig::getResponse()
+			 * @return QVariantMap HeimdallGI::ISPConfig::mResponse
+			 */
+			QVariantMap getResponse()    { return this->mConnection.getResponse(); }
+
+			/**
+			 * @paragraph This method returns the secure transport flag from the instance
+			 * @brief HeimdallGI::ISPConfig::getSecureTransportFlag()
+			 * @return bool HeimdallGI::ISPConfig::mSecureTransport
+			 */
+			bool getSecureTransportFlag()  { return this->mSecureTransport;          }
+
+			/**
+			 * @paragraph This method returns the ISPConfig server session ID
+			 * @brief HeimdallGI::ISPConfig::getSessionID()
+			 * @return QString HeimdallGI::ISPConfig::mSessionID
+			 */
+			QString getSessionID()         { return this->mSessionID;                }
+
+			///////////////////////////////////////////////////////////////////
+			/// Setters //////////////////////////////////////////////////////
+			/////////////////////////////////////////////////////////////////
+
+			/**
+			 * @paragraph This method sets the current executing SOAP method
+			 * @brief HeimdallGI::ISPConfig::setCurrentMethod()
+			 * @param HeimdallGI::ISPConfig::SoapMethod soapMethod
+			 * @return HeimdallGI::ISPConfig* HeimdallGI::ISPConfig::mInstance
+			 */
+			ISPConfig* setCurrentMethod(SoapMethod soapMethod) { this->mCurrentMethod  = soapMethod;    return this; }
+
+			/**
+			 * @paragraph This method sets the error flag
+			 * @brief HeimdallGI::ISPConfig::setErrorFlag()
+			 * @param bool blnFlag
+			 * @return HeimdallGI::ISPConfig* HeimdallGI::ISPConfig::mInstance
+			 */
+			ISPConfig* setErrorFlag(bool blnFlag)              { this->mErrorFlag      = blnFlag;       return this; }
+
+			/**
+			 * @paragraph This method sets the error message
+			 * @brief HeimdallGI::ISPConfig::setErrorMessage()
+			 * @param QString strMessage
+			 * @return HeimdallGI::ISPConfig* HeimdallGI::ISPConfig::mInstance
+			 */
+			ISPConfig* setErrorMessage(QString strMessage)     { this->mErrorMessage   = strMessage;    return this; }
+
+			/**
+			 * @paragraph This method sets the response into the instance
+			 * @brief HeimdallGI::ISPConfig::setResponse()
+			 * @param QVariantMap qvmResponse
+			 * @return HeimdallGI::ISPConfig* HeimdallGI::ISPConfig::mInstance
+			 */
+			ISPConfig* setResponse(QVariantMap qvmResponse)    { this->makeResponse    = qvmResponse;   return this; }
+
+			/**
+			 * @paragraph This method sets the secure transport flag
+			 * @brief HeimdallGI::ISPConfig::setSecureTransportFlag()
+			 * @param bool blnFlag
+			 * @return HeimdallGI::ISPConfig* HeimdallGI::ISPConfig::mInstance
+			 */
+			ISPConfig* setSecureTransportFlag(bool blnFlag)    { this->mSecureTransport = blnFlag;      return this; }
+
+			/**
+			 * This method sets the ISPConfig server session ID
+			 * @brief HeimdallGI::ISPConfig::setSessionID()
+			 * @param QString strSessionID
+			 * @return HeimdallGI::ISPConfig* HeimdallGI::ISPConfig::mInstance
+			 */
+			ISPConfig* setSessionID(QString strSessionID)      { this->mSessionID       = strSessionID; return this; }
+
+		///////////////////////////////////////////////////////////////////////
+		/// Signals //////////////////////////////////////////////////////////
+		/////////////////////////////////////////////////////////////////////
+
+		signals:
+
+			/**
+			 * @paragraph This signal fires when the logout request has finished
+			 * @brief HeimdallGI::ISPConfig::endSessionResponseReady()
+			 * @param QVariantMap &qvmResponse
+			 * @return void
+			 */
+			void endSessionResponseReady(QVariantMap &qvmResponse);
+
+			/**
+			 * @paragraph This signal fires when the login request has finished
+			 * @brief startSessionResponseReady
+			 * @param qvmResponse
+			 */
+			void startSessionResponseReady(QVariantMap &qvmResponse);
+
+
+
 
 	///////////////////////////////////////////////////////////////////////////
 	/// End HeimdallGI::ISPConfig Class Definition ///////////////////////////
